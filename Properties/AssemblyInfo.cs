d142c0982c108a22525df044a4587238f02f14aa
Nash, Duncan (Travel & Events) <P10396297@capita.co.uk>
2021-03-25 21:07:58 +0000

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("E2E_RestSharp_Example_TF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("E2E_RestSharp_Example_TF")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35317cbe-f6ea-45a5-a5fa-903de67dd8c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp; // added
+
+namespace E2E_RestSharp_Example_TF
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //var client = new RestClient("https://api.trello.com");
+            var client = new RestClient("https://api.zippopotam.us/");
+
+
+            // https://api.zippopotam.us/us/90210
+            var request = new RestRequest("us/{zipcode}", Method.GET);
+            request.AddUrlSegment("zipcode", 90210);
+
+            var content = client.Execute(request).Content;
+        }
+    }
+}
