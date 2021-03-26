16f8ce42a5bdea004a50c5343224a5ecb02cdc68
Nash, Duncan (Travel & Events) <P10396297@capita.co.uk>
2021-03-26 15:37:10 +0000

    Initial commit

---
+++ src/rest-api/Environment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2E_RestSharp_Example_TF.src.rest_api
+{
+    public class Environment
+    {
+    }
+}
---
+++ test/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Dynamitey.DynamicObjects;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp; // added
+using RestSharp.Serialization.Json;
+
+namespace E2E_RestSharp_Example_TF
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+
+        [Test]
+        public void TestMethod1()
+        {
+
+            //var client = new RestClient("https://api.trello.com");
+            var client = new RestClient("https://api.zippopotam.us/");
+
+
+            // https://api.zippopotam.us/us/90210
+            var request = new RestRequest("us/{zipcode}", Method.GET);
+            request.AddUrlSegment("zipcode", 90210);
+
+            var response = client.Execute(request);
+
+            /*
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+
+            var result = output["post code"];
+
+            Assert.That(result, Is.EqualTo("90210"), "Post code found");
+            */
+
+            JObject obs = JObject.Parse(response.Content);
+
+            Assert.That(obs["post code"].ToString(), Is.EqualTo("90210"), "Post code found");
+
+                 
+        }
+    }
+}
